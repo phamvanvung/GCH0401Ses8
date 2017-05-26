14854865abd6f473433f8542b9a6a61d4787c76e
phamvanvung <phamvanvung@gmail.com>
2017-05-26 11:17:33 +0700

    Tradition delegate approach

---
+++ SimpleLambdaExpression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleLambdaExpression
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            TraditionalDelegate();
+            Console.ReadLine();
+        }
+        public static void TraditionalDelegate(){
+			//we have a list of integers
+			List<int> list = new List<int>() { 2, 20, 1, 3, 5, 6 };
+			//Generic collection provides you a delegate called Predicate
+			Predicate<int> callback = new Predicate<int>(IsEvenNumber);
+			List<int> evenNumbers = list.FindAll(callback);
+			foreach (var item in evenNumbers)
+			{
+				Console.Write($"{item}\t");
+			}
+			Console.WriteLine();
+        }
+
+        public static bool IsEvenNumber(int number){
+            //return number % 2 == 0;
+            return number > 3;
+        }
+    }
+}
