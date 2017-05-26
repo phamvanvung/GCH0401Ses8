9077575363e75486833bc55a892124ebe93fcaee
phamvanvung <phamvanvung@gmail.com>
2017-05-26 11:23:14 +0700

    Using annonymous methods

--- SimpleLambdaExpression/Program.cs
+++ SimpleLambdaExpression/Program.cs
@@ -7,25 +7,40 @@ namespace SimpleLambdaExpression
     {
         public static void Main(string[] args)
         {
-            TraditionalDelegate();
+            //TraditionalDelegate();
+            AnnonymousMethodSyntax();
             Console.ReadLine();
         }
-        public static void TraditionalDelegate(){
+            //public static void TraditionalDelegate(){
+			////we have a list of integers
+			//List<int> list = new List<int>() { 2, 20, 1, 3, 5, 6 };
+			////Generic collection provides you a delegate called Predicate
+			//Predicate<int> callback = new Predicate<int>(IsEvenNumber);
+			//List<int> evenNumbers = list.FindAll(callback);
+			//foreach (var item in evenNumbers)
+			//{
+			//	Console.Write($"{item}\t");
+			//}
+			//Console.WriteLine();
+        //}
+		public static void AnnonymousMethodSyntax()
+		{
 			//we have a list of integers
 			List<int> list = new List<int>() { 2, 20, 1, 3, 5, 6 };
-			//Generic collection provides you a delegate called Predicate
-			Predicate<int> callback = new Predicate<int>(IsEvenNumber);
-			List<int> evenNumbers = list.FindAll(callback);
+			//Generic collection provides you a delegate called Predica
+			List<int> evenNumbers = list.FindAll(delegate(int i) {
+                return i % 2 == 0;
+            });
 			foreach (var item in evenNumbers)
 			{
 				Console.Write($"{item}\t");
 			}
 			Console.WriteLine();
-        }
+		}
 
-        public static bool IsEvenNumber(int number){
-            //return number % 2 == 0;
-            return number > 3;
-        }
+        //public static bool IsEvenNumber(int number){
+        //    //return number % 2 == 0;
+        //    return number > 3;
+        //}
     }
 }
